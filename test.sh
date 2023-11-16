ac51c5254e53ed5636e29815dd5a11811fd6c7b8
Robby Sarvis <6534331-donutsahoy@users.noreply.gitlab.com>
2023-11-16 03:24:43 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@
 for ((i=1; i<=3; i++)); do
   # curl -X GET 'http://localhost:3000/api/task/assignedToCurrentUser' \
     # -H 'Content-Type: application/json' \
-    # -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyIjp7InVzZXJfaWQiOiI1YTQ4MzNiNS0xZTZkLTRkNTEtOGUxMS1iYzk4YWUwNTQ4NDUiLCJ1c2VyX25hbWUiOiJtZWVwIiwiZW1haWwiOiJtZWVwQHllZXQuY29tIiwiaXNfYWN0aXZlIjp0cnVlLCJpc19lbWFpbF92ZXJpZmllZCI6ZmFsc2V9LCJleHAiOjE2OTMwMDMzNTV9.CQDz5zwwyvj5FnSC4cQPqH3jHTx3HPvxu5Q-MhtbzvQ'
+    # -H 'Authorization: Bearer '
 
 
 curl -X POST 'http://localhost:3000/api/v1/task/' \
 -H 'Content-Type: application/json' \
--H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VyIjp7InVzZXJfaWQiOiI5NjU3YTU2ZC05MDRhLTRiNDYtYjdiNy1lMjY5YWZmMDAzZDEiLCJ1c2VybmFtZSI6Im1lZXAiLCJlbWFpbCI6Im1lZXBAeWVldC5jb20iLCJpc19hY3RpdmUiOnRydWUsImlzX2VtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiY3JlYXRlZF9hdCI6IjIwMjMtMDktMjVUMjI6MDg6NDguNDgzMjkxWiJ9LCJleHAiOjE2OTYzMDI2MDF9.VeKIGa0cEyiwa3IIEOnbsRFEiin-FiB26_3FYR8aeP0' \
+-H 'Authorization: Bearer ' \
 -d '{
   "task_name": "waffles",
   "notes": "waffles are good",
